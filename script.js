ca8bc0a6d2880371744a79efdf89cdbdf5699359
justinZ45 <justinzaluk@gmail.com>
2022-01-07 20:30:15 -0500

    Remove columns from the grid, closes #4

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let numCol = 3; //initial number of columns
 
-function addRow()
+function addRow() //function to add row
 { 
     let table = document.getElementById("grid");  //obtains grid element and sets it to variable
    let rowClass = document.createElement("div");  //creates a new div element
@@ -16,23 +16,34 @@ function addRow()
 }
 
  
-function addCol()
+function addCol() //function to add column
 {
-  let columns = document.getElementsByClassName("row");
+  let columns = document.getElementsByClassName("row"); //obtains array of row elements, set to columns variable
  
-   for(let i = 0; i < columns.length; i++)
+   for(let i = 0; i < columns.length; i++) //traverses columns array
    {
-       let newCol = document.createElement("div");
-      newCol.setAttribute("class", "col-sm border border-dark box white");
-      columns[i].appendChild(newCol);
+       let newCol = document.createElement("div");  //creates new div element
+      newCol.setAttribute("class", "col-sm border border-dark box white"); //sets attributes of div element
+      columns[i].appendChild(newCol); //appends new column to grid
       
    }
-   numCol++;
+   numCol++; //increases column count
 }
 
-function removeRow() 
+function removeRow() //function to remove row 
 {
-let delRows = document.getElementsByClassName("row");
-delRows[0].remove();
+let delRows = document.getElementsByClassName("row");  //array of row elements 
+delRows[0].remove(); //removes first row element of grid
 }
 
+ 
+function removeCol() //function to remove column
+{
+   let delColumns = document.getElementsByClassName("row");  //row array set equal to variable
+   for(let i = 0; i < delColumns.length; i++) //traverses delColumns array
+   {
+           delColumns[i].removeChild(delColumns[i].lastElementChild); //removes last column of each group of rows
+ 
+   }
+   numCol--; //decreases column count by one
+}
